00b185b3e88a1409f744cb01b9b6975932126967
Rafael <rafael.gcaldas01@gmail.com>
2022-12-13 22:22:28 -0300

    New folders

---
+++ src/application/entities/notifications.ts
@@ -0,0 +1,51 @@
+export interface NotificationProps {
+  recepientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public set recepientId(recepientId: string) {
+    this.props.recepientId = recepientId;
+  }
+
+  public get recepientId(): string {
+    return this.props.recepientId;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
