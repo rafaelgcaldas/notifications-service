5015404caf8827b8f8c5c68d56871ea66d30e102
Rafael <rafael.gcaldas01@gmail.com>
2022-12-21 23:30:22 -0300

    Test - cancel a notification that does not exist

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,6 +2,7 @@ import { Content } from "@application/entities/content";
 import { Notification } from "@application/entities/notification";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
 import { CancelNotification } from "./cancel-notification";
+import { NotificationNotFound } from "./errors/notification-not-found";
 
 
 describe("Cancel notification", () => {
@@ -24,6 +25,16 @@ describe("Cancel notification", () => {
     expect(notificationRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date)
     );
+  })
 
+  it("should not be able to cancel a non existing a notification", async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notficationId: "fake-notification-id"
+      });
+    }).rejects.toThrow(NotificationNotFound);
   })
 })
